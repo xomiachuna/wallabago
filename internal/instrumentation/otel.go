95b8ac5f256b8aa7fe18783bb740260ed460d265
Andrii Khomiak <andrew.hamster.1st@gmail.com>
2025-07-16 19:11:29 +0300

    feat: Add otel sdk setup to instrumentation

--- internal/instrumentation/otel.go
+++ internal/instrumentation/otel.go
@@ -1,10 +1,25 @@
 package instrumentation
 
 import (
+	"context"
 	"runtime/debug"
 
 	"go.opentelemetry.io/otel"
 
+	stderrors "errors"
+
+	"github.com/pkg/errors"
+	"go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploggrpc"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	otellog "go.opentelemetry.io/otel/log"
+	otelgloballog "go.opentelemetry.io/otel/log/global"
+	"go.opentelemetry.io/otel/propagation"
+	sdklog "go.opentelemetry.io/otel/sdk/log"
+	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
+	sdkresource "go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
 	"go.opentelemetry.io/otel/trace"
 )
 
@@ -23,4 +38,136 @@ func initTracer() {
 
 func init() {
 	initTracer()
+}
+
+func newPropagator() propagation.TextMapPropagator {
+	return propagation.NewCompositeTextMapPropagator(
+		propagation.Baggage{},
+		propagation.TraceContext{},
+	)
+}
+
+func newResource(ctx context.Context) (*sdkresource.Resource, error) {
+	baseResource, err := sdkresource.Merge(
+		sdkresource.Default(),
+		sdkresource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceName("wallabago-api"),
+		),
+	)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	extendedInfo, err := sdkresource.New(
+		ctx,
+		sdkresource.WithOS(),
+		sdkresource.WithContainer(),
+		sdkresource.WithContainerID(),
+	)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	merged, err := sdkresource.Merge(extendedInfo, baseResource)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	return merged, nil
+}
+
+func newTracerProvider(ctx context.Context, resource *sdkresource.Resource) (*sdktrace.TracerProvider, error) {
+	remoteExporter, err := otlptracegrpc.New(ctx)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	return sdktrace.NewTracerProvider(
+		sdktrace.WithResource(resource),
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithBatcher(remoteExporter),
+	), nil
+}
+
+func newMeterProvider(ctx context.Context, resource *sdkresource.Resource) (*sdkmetric.MeterProvider, error) {
+	remoteExporter, err := otlpmetricgrpc.New(ctx)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	return sdkmetric.NewMeterProvider(
+		sdkmetric.WithResource(resource),
+		sdkmetric.WithReader(sdkmetric.NewPeriodicReader(remoteExporter)),
+	), nil
+}
+
+// scopeInjectingSpanProcessor is a [sdklog.Processor]
+// that injects [instrumentation.Scope] via [attribute]s using [semconv].
+type scopeInjectingLogProcessor struct{}
+
+func (lp *scopeInjectingLogProcessor) OnEmit(_ context.Context, record *sdklog.Record) error {
+	scope := record.InstrumentationScope()
+	record.SetAttributes(
+		otellog.String(string(semconv.OTelScopeNameKey), scope.Name),
+		otellog.String(string(semconv.OTelScopeVersionKey), scope.Version),
+	)
+	return nil
+}
+func (lp *scopeInjectingLogProcessor) Shutdown(_ context.Context) error   { return nil }
+func (lp *scopeInjectingLogProcessor) ForceFlush(_ context.Context) error { return nil }
+
+func newLoggerProvider(ctx context.Context, resource *sdkresource.Resource) (*sdklog.LoggerProvider, error) {
+	remoteExporter, err := otlploggrpc.New(ctx)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	return sdklog.NewLoggerProvider(
+		sdklog.WithResource(resource),
+		sdklog.WithProcessor(&scopeInjectingLogProcessor{}),
+		sdklog.WithProcessor(sdklog.NewBatchProcessor(remoteExporter)),
+	), nil
+}
+
+func SetupOtelSDK(ctx context.Context) (shutdownOtel func(ctx context.Context) error, err error) {
+	// holds the callbacks necessary for proper otel shutdown
+	doOtelShutdownCallbacks := make([]func(context.Context) error, 0)
+
+	// properly shutdown everything set up by otel
+	// by calling every callback
+	shutdownOtel = func(ctx context.Context) error {
+		var shutdownErr error
+		for i, callback := range doOtelShutdownCallbacks {
+			if err := callback(ctx); err != nil {
+				shutdownErr = errors.Wrapf(err, "error when calling cancel callback #%d", i)
+			}
+		}
+		return shutdownErr
+	}
+
+	propagator := newPropagator()
+	otel.SetTextMapPropagator(propagator)
+
+	resource, err := newResource(ctx)
+	if err != nil {
+		return nil, stderrors.Join(err, shutdownOtel(ctx))
+	}
+
+	tracerProvider, err := newTracerProvider(ctx, resource)
+	if err != nil {
+		return nil, stderrors.Join(err, shutdownOtel(ctx))
+	}
+	otel.SetTracerProvider(tracerProvider)
+	doOtelShutdownCallbacks = append(doOtelShutdownCallbacks, tracerProvider.Shutdown)
+
+	loggerProvider, err := newLoggerProvider(ctx, resource)
+	if err != nil {
+		return nil, stderrors.Join(err, shutdownOtel(ctx))
+	}
+	otelgloballog.SetLoggerProvider(loggerProvider)
+	doOtelShutdownCallbacks = append(doOtelShutdownCallbacks, loggerProvider.Shutdown)
+
+	meterProvider, err := newMeterProvider(ctx, resource)
+	if err != nil {
+		return nil, stderrors.Join(err, shutdownOtel(ctx))
+	}
+	otel.SetMeterProvider(meterProvider)
+	doOtelShutdownCallbacks = append(doOtelShutdownCallbacks, meterProvider.Shutdown)
+
+	return shutdownOtel, nil
 }
